2b1da2521f665f015eaeee57740fc5f2785699a2
rlakhno <rouslanlakhno@gmail.com>
2022-10-18 20:42:12 -0400

    First commit, trying to create executable jar

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       ConvertToJson convertToJson = new ConvertToJson();
+       MyName myName = new MyName("Rouslan", "Lakhno");
+       String text = convertToJson.toJson(myName);
+        System.out.println(text);
+    }
+}
